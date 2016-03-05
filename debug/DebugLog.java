42814dea5f9de5dd98b06b13d1711f83b772612d
WJRye <1126594968@qq.com>
2016-03-05 19:05:41 +0800

    添加默认的Log模式

--- debug/DebugLog.java
+++ debug/DebugLog.java
@@ -116,4 +116,94 @@ public final class DebugLog extends Debug {
 		return IS_PUBLIC_DEBUG && IS_PRIVATE_DEBUG;
 	}
 
+	public static void v(String msg) {
+
+		if (isDebug())
+			Log.v(TAG, msg);
+
+	}
+
+	public static void v(String msg, Throwable tr) {
+
+		if (isDebug())
+			Log.v(TAG, msg, tr);
+
+	}
+
+	public static void d( String msg) {
+
+		if (isDebug())
+			Log.d(TAG, msg);
+
+	}
+
+	public static void d(String msg, Throwable tr) {
+
+		if (isDebug())
+			Log.d(TAG, msg, tr);
+
+	}
+
+	public static void i(String msg) {
+
+		if (isDebug())
+			Log.i(TAG, msg);
+
+	}
+
+	public static void i(String msg, Throwable tr) {
+
+		if (isDebug())
+			Log.i(TAG, msg, tr);
+
+	}
+
+	public static void w(String msg) {
+
+		if (isDebug())
+			Log.w(TAG, msg);
+
+	}
+
+	public static void w(String msg, Throwable tr) {
+
+		if (isDebug())
+			Log.w(TAG, msg, tr);
+
+	}
+
+	public static void e( String msg) {
+
+		if (isDebug())
+			Log.e(TAG, msg);
+
+	}
+
+	public static void e(String msg, Throwable tr) {
+
+		if (isDebug())
+			Log.e(TAG, msg, tr);
+
+	}
+
+	public static void wtf( String msg) {
+
+		if (isDebug())
+			Log.wtf(TAG, msg);
+
+	}
+
+	public static void wtf( String msg, Throwable tr) {
+
+		if (isDebug())
+			Log.wtf(TAG, msg, tr);
+
+	}
+
+	public static void println(int priority, String msg) {
+
+		if (isDebug())
+			Log.println(priority, TAG, msg);
+
+	}
 }
